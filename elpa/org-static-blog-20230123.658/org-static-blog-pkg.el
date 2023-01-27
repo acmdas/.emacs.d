9a4bc6db9793124119b397366281bf9d3092fb5e
David <das@acm.org>
2023-01-27 09:47:47 -0500

    Updated org-journal and org-static-blog

---
+++ elpa/org-static-blog-20230123.658/org-static-blog-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-static-blog.el  -*- no-byte-compile: t -*-
+(define-package "org-static-blog" "20230123.658" "a simple org-mode based static blog generator" '((emacs "24.3")) :commit "9d3dffddad4a7ebeb621e03ceb820ed0f1a09a4d" :authors '(("Bastian Bechtold")) :maintainer '("Bastian Bechtold") :url "https://github.com/bastibe/org-static-blog")
